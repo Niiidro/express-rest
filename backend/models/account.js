8a0d63d80cd7f35282eb4b730d92e80541262bea
Philipp Minder <philipp.minder@hotmail.com>
2021-09-27 02:25:14 +0200

    Improve get and post Functions. Create Hashed Functions in Schema

--- backend/models/account.js
+++ backend/models/account.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const { Schema } = mongoose;
 
-const accountSchema = new Schema(
+const AccountSchema = new Schema(
     {
         name: {
             type: String,
@@ -32,4 +33,26 @@ const accountSchema = new Schema(
     }
 );
 
-export default mongoose.model('Account', accountSchema);
+//Hash Password when save Account https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+AccountSchema.pre('save', function (next) {
+    let user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+//Compare Hashed Password Function https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+AccountSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);  
+    });
+};
+
+
+export default mongoose.model('Account', AccountSchema);
--- backend/routes/accountRoute.js
+++ backend/routes/accountRoute.js
@@ -1,27 +1,81 @@
-import express from 'express';
+import express, { response } from 'express';
+import bcrypt from 'bcrypt';
 import Account from '../models/account.js';
+import { checkMail, checkPassword } from '../middleware.js';
 
 const router = express.Router();
 
 //Get all Accounts
 router.get('/account', async (req, res) => {
-    const accounts = await Account.find();
-    res.send(accounts);
+    Account.find(function (err, doc) {
+        if (doc.length > 0) {
+            res.status(200).send(doc);
+        } else if (doc.length === 0) {
+            res.status(404).send('No Accounts found');
+        }
+    });
+});
+
+//Get one Account
+router.get('/account/:id', async (req, res) => {
+    const id = req.params.id;
+    Account.findById(id, function (err, doc) {
+        if (err) {
+            res.status(500).send('A Problem occured while searching Account.');
+        } else if (doc) {
+            res.status(200).send(doc);
+        } else {
+            res.status(404).send('No Account found with this ID');
+        }
+    });
 });
 
 //Create Account
-router.post('/account', async (req, res) => {
-    const account = new Account({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
+router.post('/account', checkMail, checkPassword, async (req, res) => {
+    const body = req.body;
+    Account.exists({ email: body.email }, function (err, doc) {
+        if (!doc) {
+            const newAcc = new Account(body);
+            newAcc.save(function (err, doc) {
+                if (err) {
+                    res.status(500).send(
+                        'A Problem occured while creating Account.'
+                    );
+                }
+                res.status(201)
+                    .location(`/account/${doc._id}`)
+                    .send('Account successfully created.');
+            });
+        } else {
+            res.status(409).send('Accout with this E-Mail already exist');
+        }
     });
-    const existAccount = await Account.exists({ email: account.email });
-    if (!existAccount) {
-        await account.save();
-        res.status(200).send(account);
-    } else {
-        res.status(409).send('Account with this E-Mail already exists');
+});
+
+//Update Account
+router.patch('/account/:id', async (req, res) => {
+    const id = req.params.id;
+    const body = req.body;
+    try {
+        const account = await Account.findByIdAndUpdate(id, body, {
+            new: true,
+        });
+        res.status(200).send('User updated');
+    } catch (e) {
+        res.status(404).send('Account with this ID not found');
+        console.log(e);
+    }
+});
+
+//Delete Account
+router.delete('/account/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await Account.deleteOne({ _id: id });
+        res.status(200).send('Account deleted');
+    } catch (e) {
+        res.status(404).send('Account with this ID not found');
+        console.log(e);
     }
 });
 
